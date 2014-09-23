8189203731bbb03c116faccee7a8a91739e78a40
Robert Lee Brackett III <robert.brackett3@gmail.com>
2014-09-23 14:38:24 -0400

    Update 20140921182649_create_posts.rb

--- db/migrate/20140921182649_create_posts.rb
+++ db/migrate/20140921182649_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :text
+      t.string :text #you may want this to be a text object => t.text :text since it could be larger than 255 hars
       t.integer :user_id
 
       t.timestamps
